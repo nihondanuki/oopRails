e28fabb8614ed5b3676e1d364929203141767e2c
takuotuka <otsuka.taku@mwed.co.jp>
2018-06-01 15:54:21 +0900

    add refectering spec user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "user valid?" do
+    subject { User.new(params) }
+    context "user is valid" do
+      let(:params) {{email: "a@a.com", password: "foobar", password_confirmation: "foobar"}}
+      it {is_expected.to be_valid}
+    end
+    context "email is invalid" do
+      let(:params) {{password: "foobar", password_confirmation: "foobar"}}
+      it {is_expected.to be_invalid}
+    end
+    context "password is invalid" do
+      let(:params) {{email: "a@a.com"}}
+      it {is_expected.to be_invalid}
+    end
+    context "user in not unique" do
+      let(:params) {{email: "a@a.com", password: "foobar", password_confirmation: "foobar"}}
+      before do
+        User.new(params).save
+      end
+      it {is_expected.to be_invalid}
+    end
+  end
 end
